71b1e626944a3fdbf09ee4cd295f9aaacce682f1
Patrick BEUGRE <pt.beugre@gmail.com>
2020-03-23 13:38:32 +0100

    deploy to tomcat

--- src/main/java/ci/pabeu/email/controller/EmailController.java
+++ src/main/java/ci/pabeu/email/controller/EmailController.java
@@ -4,11 +4,12 @@ import java.io.UnsupportedEncodingException;
 
 import javax.mail.MessagingException;
 
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +33,9 @@ public class EmailController {
 		return emailServiceImpl.send(email);
 	}
 	
+	@GetMapping(name = "/testJenkins", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> get() {
+		return new ResponseEntity<String>("Okay for jenkins", HttpStatus.ACCEPTED);
+	}
 
 }
