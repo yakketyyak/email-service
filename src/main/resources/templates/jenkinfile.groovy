846cdafdfaa0a1aef51788c87b140422be5dd7a5
Patrick BEUGRE <pt.beugre@gmail.com>
2020-03-24 19:29:18 +0100

    update file according to ok and nok

--- src/main/resources/templates/jenkinfile.groovy
+++ src/main/resources/templates/jenkinfile.groovy
@@ -1,3 +1,4 @@
+//OKAY
 pipeline {
    
 	 agent any
@@ -47,7 +48,7 @@ pipeline {
         }
     }
 }
-
+//OKAY
 pipeline {
    
     agent none
@@ -134,7 +135,7 @@ pipeline {
             }
     }
 }
-
+//OKAY
 pipeline {
    
    agent any
@@ -223,7 +224,7 @@ pipeline {
         }
     }
 }
-
+//OKAY
 pipeline {
    
    agent any
@@ -272,7 +273,7 @@ pipeline {
 
     }
 }
-
+//OKAY
 node {
 
     stage('Main Build') { 
@@ -294,55 +295,55 @@ node {
         }
       }
     }
-
-    pipeline {
-    agent any
-    stages
-    {
-      stage('Docker Build') { 
-      steps{
-        script{
-          docker.image('maven:3.6-jdk-8').inside ('-v $HOME/.m2:/root/.m2'){
-        
-          stage('Build'){
-            git 'https://github.com/yakketyyak/jenkins.git'
-            sh '''
-               mvn -v
-               mvn -B -DskipTests clean package
-            '''
-          }
-
-          stage('Test'){
-            sh '''
-              mvn test
-              echo $WORKSPACE
-            '''
-          }
+//OKAY
+pipeline {
+agent any
+stages
+{
+  stage('Docker Build') { 
+  steps{
+    script{
+      docker.image('maven:3.6-jdk-8').inside ('-v $HOME/.m2:/root/.m2'){
+    
+      stage('Build'){
+        git 'https://github.com/yakketyyak/jenkins.git'
+        sh '''
+           mvn -v
+           mvn -B -DskipTests clean package
+        '''
       }
-    }
-   }
-      
+
+      stage('Test'){
+        sh '''
+          mvn test
+          echo $WORKSPACE
+        '''
+      }
   }
-    stage('SSH transfer'){
-          steps([$class: 'BapSshPromotionPublisherPlugin']){
-            sshPublisher(
-                continueOnError: false, failOnError: true,
-                publishers: [
-                    sshPublisherDesc(
-                        configName: 'localhost',
-                        verbose: true,
-                        transfers: [
-                            sshTransfer(
-                              sourceFiles: '**/*spring-test-0.0.1-SNAPSHOT.jar',
-                              execCommand: 'mv deployJenkins/target/spring-test-0.0.1-SNAPSHOT.jar deployJenkins/target/spring-test-0.0.1-SNAPSHOT.jar'
-                            )
-                        ],
-                        useWorkspaceInPromotion: true,
-                        usePromotionTimestamp: true
-                    )
-                ]
-            )
-          }
-    }
-   } 
+}
+}
+  
+}
+stage('SSH transfer'){
+      steps([$class: 'BapSshPromotionPublisherPlugin']){
+        sshPublisher(
+            continueOnError: false, failOnError: true,
+            publishers: [
+                sshPublisherDesc(
+                    configName: 'localhost',
+                    verbose: true,
+                    transfers: [
+                        sshTransfer(
+                          sourceFiles: '**/*spring-test-0.0.1-SNAPSHOT.jar',
+                          execCommand: 'mv deployJenkins/target/spring-test-0.0.1-SNAPSHOT.jar deployJenkins/target/spring-test-0.0.1-SNAPSHOT.jar'
+                        )
+                    ],
+                    useWorkspaceInPromotion: true,
+                    usePromotionTimestamp: true
+                )
+            ]
+        )
+      }
+}
+} 
 }
