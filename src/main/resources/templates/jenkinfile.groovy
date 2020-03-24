c73b2cf45dd929d4d26c2e5ce446edf48e50415d
Patrick BEUGRE <pt.beugre@gmail.com>
2020-03-24 15:02:33 +0100

    workspace and ssh

--- src/main/resources/templates/jenkinfile.groovy
+++ src/main/resources/templates/jenkinfile.groovy
@@ -272,3 +272,25 @@ pipeline {
 
     }
 }
+
+node {
+
+    stage('Main Build') { 
+      docker.image('maven:3.6-jdk-8').inside ('-v $HOME/.m2:/root/.m2'){
+        
+        stage('Build'){
+          git 'https://github.com/yakketyyak/jenkins.git'
+          sh '''
+             mvn -v
+             mvn -B -DskipTests clean package
+          '''
+        }
+
+        stage('Test'){
+          sh '''
+            mvn test
+            echo $WORKSPACE
+          '''
+        }
+      }
+    }
